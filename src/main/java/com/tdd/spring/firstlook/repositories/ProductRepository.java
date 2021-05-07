58aa7dd241464c6985937d03285a3b99a8eab3b8
Joan Pedro <jpos.senai@gmail.com>
2021-05-07 10:42:41 -0300

    feat: reviews finder methods, using specification names

--- src/main/java/com/tdd/spring/firstlook/repositories/ProductRepository.java
+++ src/main/java/com/tdd/spring/firstlook/repositories/ProductRepository.java
@@ -3,5 +3,21 @@ package com.tdd.spring.firstlook.repositories;
 import com.tdd.spring.firstlook.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+  List<Product> findByName(String name);
+
+  List<Product> findByNameAndDesc(String name, String desc);
+
+  List<Product> findByDescLike(String desc);
+
+  List<Product> findByDescContains(String desc);
+
+  List<Product> findByPriceGreaterThan(Double price);
+
+  List<Product> findByPriceBetween(Double price1, Double price2);
+
+  List<Product> findByIdIn(List<Integer> ids);
 }
--- src/test/java/com/tdd/spring/firstlook/repositories/ProductRepositoryTest.java
+++ src/test/java/com/tdd/spring/firstlook/repositories/ProductRepositoryTest.java
@@ -4,19 +4,24 @@ import com.tdd.spring.firstlook.domain.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ProductRepositoryTest {
 
   @Autowired
   ProductRepository productRepository;
 
   @Test
+  @Transactional
   void testCreateAndFind() {
     Product product = new Product(null, "Computer", "An awesome Laptop", 3300.00);
     productRepository.save(product);
@@ -25,6 +30,7 @@ class ProductRepositoryTest {
   }
 
   @Test
+  @Transactional
   void testUpdateAndFind() {
     Product product = new Product(null, "Computer", "An awesome Laptop", 3300.00);
     productRepository.save(product);
@@ -40,6 +46,7 @@ class ProductRepositoryTest {
   }
 
   @Test
+  @Transactional
   void testDeleteAndFind() {
     Product product = new Product(null, "Computer", "An awesome Laptop", 3300.00);
     productRepository.save(product);
@@ -54,6 +61,7 @@ class ProductRepositoryTest {
   }
 
   @Test
+  @Transactional
   void testCount() {
     Product product1 = new Product(null, "Computer", "An awesome Laptop", 3300.00);
     Product product2 = new Product(null, "Table", "An great table", 3300.00);
@@ -63,5 +71,120 @@ class ProductRepositoryTest {
     long counter = productRepository.count();
 
     assertEquals(3, counter);
+  }
+
+  @Test
+  @Transactional
+  void testCustomListByName() {
+    Product p1 = new Product(null, "Computer", "An awesome Laptop", 3300.00);
+    Product p2 = new Product(null, "Table", "An great table", 3300.00);
+    Product p3 = new Product(null, "Chair", "It's confortable", 3300.00);
+
+    productRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+    List<Product> pByName = productRepository.findByName("Table");
+
+    pByName.forEach(product -> {
+      assertEquals("Table", product.getName());
+    });
+  }
+
+  @Test
+  @Transactional
+  void testCustomListByNameAndDesc() {
+    Product p1 = new Product(null, "Computer", "An awesome Laptop", 3300.00);
+    Product p2 = new Product(null, "Table", "An great table", 3300.00);
+    Product p3 = new Product(null, "Chair", "It's confortable", 3300.00);
+
+    productRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+    List<Product> pByNameAndDesc = productRepository.findByNameAndDesc("Table", "An great table");
+
+    pByNameAndDesc.forEach(product -> {
+      assertEquals("Table", product.getName());
+      assertEquals("An great table", product.getDesc());
+    });
+  }
+
+  @Test
+  @Transactional
+  void testCustomListByDescLike() {
+    Product p1 = new Product(null, "Computer", "An awesome Laptop", 3300.00);
+    Product p2 = new Product(null, "Table", "An great table", 3300.00);
+    Product p3 = new Product(null, "Chair", "It's confortable", 3300.00);
+
+    productRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+    List<Product> pByDescLike = productRepository.findByDescLike("An");
+
+    pByDescLike.forEach(product -> {
+      if (product.getName().equals("Computer")) {
+        assertEquals("An awesome Laptop", product.getDesc());
+      } else {
+        assertEquals("An great table", product.getDesc());
+      }
+    });
+  }
+
+  @Test
+  @Transactional
+  void testCustomListByDescContains() {
+    Product p1 = new Product(null, "Computer", "An awesome Laptop", 3300.00);
+    Product p2 = new Product(null, "Table", "An great table", 3300.00);
+    Product p3 = new Product(null, "Chair", "It's confortable", 3300.00);
+
+    productRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+    List<Product> pByDescLike = productRepository.findByDescContains("An");
+
+    pByDescLike.forEach(product -> {
+      if (product.getName().equals("Computer")) {
+        assertEquals("An awesome Laptop", product.getDesc());
+      } else {
+        assertEquals("An great table", product.getDesc());
+      }
+    });
+  }
+
+  @Test
+  @Transactional
+  void testCustomListByPriceGreaterThan() {
+    Product p1 = new Product(null, "Computer", "An awesome Laptop", 3000.00);
+    Product p2 = new Product(null, "Table", "An great table", 2100.00);
+    Product p3 = new Product(null, "Chair", "It's confortable", 3300.00);
+
+    productRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+    List<Product> pByPrice = productRepository.findByPriceGreaterThan(2999.99);
+
+    assertEquals(2, pByPrice.size());
+  }
+
+  @Test
+  @Transactional
+  void testCustomListByPriceBetween() {
+    Product p1 = new Product(null, "Computer", "An awesome Laptop", 3000.00);
+    Product p2 = new Product(null, "Table", "An great table", 2100.00);
+    Product p3 = new Product(null, "Chair", "It's confortable", 3300.00);
+
+    productRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+    List<Product> pByPrice = productRepository.findByPriceBetween(2999.99, 3400.00);
+
+    assertEquals(2, pByPrice.size());
+  }
+
+  @Test
+  @Transactional
+  void testCustomListByIdsIn() {
+    Product p1 = new Product(null, "Computer", "An awesome Laptop", 3000.00);
+    Product p2 = new Product(null, "Table", "An great table", 2100.00);
+    Product p3 = new Product(null, "Chair", "It's confortable", 3300.00);
+
+    productRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+    List<Product> pByPrice = productRepository.findByIdIn(Arrays.asList(1, 2));
+
+    assertEquals(2, pByPrice.size());
   }
 }
